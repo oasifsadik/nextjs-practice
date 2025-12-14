a2dfea8ad2b06e1d1602391be623372bb93c69f9
oasifsadik <oasifsadik99@gmail.com>
2025-12-14 21:20:09 +0600

    routing metadata

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,8 +1,19 @@
+
+
+type Props = {
+    params: {productId: string}
+}
+ export const generateMetadata = async ({
+    params
+}: Props) => {
+    const productId = (await params).productId;
+    return {
+        title: `Product ${productId} Details`,
+    }
+ }
 export default async function products( {
     params
-}: {
-    params: {productId: string}
-})
+}: Props)
     {
     const productId = (await params).productId;
     return <h1>product details {productId} </h1>;
